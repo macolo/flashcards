e32691db340fb840e1e393df9e6e9f616a719477
Mario Colombo <mario.colombo@webrepublic.ch>
2014-11-30 23:11:37 +0100

    added cardlist add / remove, fixed some auth stuff

--- cards/urls.py
+++ cards/urls.py
@@ -4,8 +4,11 @@ from cards import views
 
 # /cardlists/...
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.cardlist_index, name='cardlist_index'),
     url(r'^(?P<cardlist_id>[0-9]+)/$', views.cardlist, name='cardlist'),
+    url(r'^new$', views.new_cardlist, name='newcardlist'),
     url(r'^(?P<cardlist_id>[0-9]+)/(?P<card_id>[0-9]+)/$', views.card, name='card'),
-    url(r'^(?P<cardlist_id>[0-9]+)/addcard$', views.add_cart_to_cardlist, name='addcard'),
+    url(r'^(?P<cardlist_id>[0-9]+)/addcard$', views.add_card_to_cardlist, name='addcard'),
+    url(r'^(?P<cardlist_id>[0-9]+)/deletecardlist$', views.delete_cardlist, name='deletecardlist'),
+
 ]
