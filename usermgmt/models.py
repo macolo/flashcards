b92c010279634d9d30469eb1cb38dd59b63ef176
Mario Colombo <mario.colombo@webrepublic.ch>
2015-01-03 19:05:06 +0100

    hotfix to make email validation hashes be unique

--- usermgmt/models.py
+++ usermgmt/models.py
@@ -5,12 +5,17 @@ from binascii import hexlify
 
 # Create your models here.
 
+# I don' like this, would rather have this in the model class but heck...
+# http://stackoverflow.com/questions/3573834/django-fields-default-value-from-self-models-instances
+def generate_random_hash():
+    return uuid.uuid1().hex
+
 # This stores the hashes for email validation
 class UserValidationCode(models.Model):
     created_date = models.DateTimeField('date published', auto_now_add=True)
     user = models.ForeignKey(User)
     # The hash is unique and auto-generated for an existing user
-    hash = models.CharField(max_length=200, blank=True, default=uuid.uuid1().hex, editable=False)
+    hash = models.CharField(max_length=200, blank=True, default=generate_random_hash, editable=False, unique=True)
 
     def get_user_name(self):
         return self.user.username
@@ -20,3 +25,4 @@ class UserValidationCode(models.Model):
 
     def __unicode__(self):
         return self.user.username
+
