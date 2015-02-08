5b7541958d747051361a7147a36c242b44e9f3c3
Mario Colombo <mario.colombo@webrepublic.ch>
2015-02-09 00:20:15 +0100

    first card edit version

--- cards/views.py
+++ cards/views.py
@@ -5,7 +5,7 @@ import logging
 from django.contrib.auth.models import User, Group
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.utils import timezone
@@ -423,6 +423,29 @@ def get_list_of_allowed_cardlists(request, at_least_mode):
                     continue
 
             return cardlist_list_filtered_by_mode
+
+@login_required
+def update_card(request, card_id):
+    # this is a async endpoint
+    # meaning it does not return any rendered HTML
+    if request.method =='POST':
+        try:
+            question = request.POST['question']
+            answer = request.POST['answer']
+        except KeyError:
+            return HttpResponseServerError()
+
+        card = Card.objects.get(pk=card_id)
+        card.card_question = question
+        card.card_answer = answer
+        card.save()
+
+        # all ok
+        return HttpResponse()
+
+
+    else:
+        return HttpResponseServerError()
 
 
 @login_required
