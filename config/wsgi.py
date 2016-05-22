0b35482a65f95bd72643a1b00c298308b95f2067
Mario Colombo <mario@what.digital>
2016-05-22 20:16:41 +0200

    fix

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,20 +10,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 # https://github.com/jpadilla/django-dotenv
 import os
 import dotenv
+from django.core.wsgi import get_wsgi_application
+
+
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
-from django.core.wsgi import get_wsgi_application
-import config.settings
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-# only if not locally executed
-if not config.settings.LOCAL:
-
-    # Activate the virtual env
-    activate_virtual_env_path = config.settings.require_env('ME_VIRTUALENV_ACTIVATE_THIS_PATH')
-    with open(activate_virtual_env_path) as f:
-        code = compile(f.read(), activate_virtual_env_path, 'exec')
-        exec(code)
 
 application = get_wsgi_application()
